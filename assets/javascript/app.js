5e9bc4aadc5e74f07411e214b5588b22ac4beb7b
Cesar Pocai <c_pocai1hotmail.com@Cesars-MacBook.local>
2017-12-18 15:18:53 -0500

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,89 @@
-$(document).ready()
-//variable - starting point count down number
-var startNumber = 30;
-// variable - holds the time after which we subtract 1 from the startNumber. In our case 1 second
-var intervalId = 0;
-//variable - results
+/////////////////--Variables--/////////////////
+var Title = "<h1>Trivial Trivia Game!</h1>"
+var BeginButton = "<button class='btn btn-primary btn-lg' id='BeginButton' role='button'>Begin Trivia Game!</button>"
+var BeginNewButton = "<button class='btn btn-primary btn-lg' id='BeginNewButton' role='button'>Begin New Trivia Game!</button>"
+var SubmitAnswer = "<button class='btn btn-primary btn-lg' id='SubmitAnswer' role='button'>Submit Answer</button>"
+var StartNumber = 20;
+var IntervalId = 0;
 var win = 0;
 var lose = 0;
-//variable - question 1.
 var q1 = {
   q: "What is q1?",
   a1: "<input type='radio' name='gender1' id='Correct' value='Q1Male'>  Q1Male  ",
   a2: "<input type='radio' name='gender1' id='Q1Female' value='Q1female'>  Q1Female  ",
   a3: "<input type='radio' name='gender1' id='Q1Other' value='Q1other'>  Q1Other  "
 };
-//variable - question 2.
 var q2 = {
   q: "What is q2?",
   a1: "<input type='radio' name='gender2' id='Q2Male' value='Q2male'>  Q2Male  ",
   a2: "<input type='radio' name='gender2' id='Correct' value='Q2Female'>  Q2Female  ",
   a3: "<input type='radio' name='gender2' id='Q2Other' value='Q2other'>  Q2Other  "
 };
-//variable - question 3.
 var q3 = {
   q: "What is q3?",
   a1: "<input type='radio' name='gender3' id='Q3Male' value='Q3male'>  Q3Male  ",
   a2: "<input type='radio' name='gender3' id='Q3female' value='Q3female'>  Q3Female  ",
   a3: "<input type='radio' name='gender3' id='Correct' value='Q3Other'>  Q3Other  "
 };
-//variable - question 4.
 var q4 = {
   q: "What is q4?",
   a1: "<input type='radio' name='gender4' id='Correct' value='Q4Male'>  Q4Male  ",
   a2: "<input type='radio' name='gender4' id='Q4female' value='Q4female'>  Q4Female  ",
   a3: "<input type='radio' name='gender4' id='Q4Other' value='Q4other'>  Q4Other  "
 };
-//variable - Array to hold questions
 var questions = [q1,q2,q3,q4];
-//Action - when user clicks the stop button the "stop" function is executed.
-$("#stop").on("click", stop);
-//function - execute the start function which that sets the intervalId to 1 second and excutes the subtract function every 1 second.
-function start (){
-  intervalId = setInterval(subtract, 1000);
+
+/////////////////--Function--/////////////////
+function start(){
+  $("#jumbotron").html("<div class='row'><div class='col-8'>"+Title+"</div><div class='col-4' id='col-4'></div></div><hr>");
+  $("#jumbotron").append("<div class='row'><div class='col-12' id='col-12'>"+BeginButton+"</div></div>");
+  $("#BeginButton").on("click", game);
 };
-//function - stop the interval count down, and redirects user to result page.
+function game(){
+  IntervalId = setInterval(subtract, 1000);
+  getRandom();
+};
+function subtract(){
+  StartNumber--;
+  if (StartNumber === 0){
+  stop();
+  $("#jumbotron").html("<div class='row'><div class='col-12'>"+Title+"</div><hr><div class='col-12'>You have answered "+win+" questions correctly!</div><div class='col-12'>You have answered "+lose+" questions incorrectly!</div><div class='col-12' id='col-12'>"+BeginNewButton+"</div></div>");
+  $("#BeginNewButton").on("click", reStart);
+  }
+  $("#col-4").html(StartNumber);
+};
+function getRandom(){
+  var i = Math.floor((Math.random() * 4) + 0);
+  $("#col-12").html("<div class='question'>"+questions[i].q+"</div><div class='answer'>"+questions[i].a1+questions[i].a2+questions[i].a3+"</div><div class='submitanswer'>"+SubmitAnswer+"</div>");
+  $("#SubmitAnswer").on("click", stop);
+};
 function stop() {
-  clearInterval(intervalId);
+  clearInterval(IntervalId);
   var x = document.getElementById('Correct').checked;
   if (x === true){
     win++;
+      if (StartNumber > 0){
+        game();
+      } else {
+        $("#jumbotron").html("<div class='row'><div class='col-12'>"+Title+"</div><hr><div class='col-12'>You have answered "+win+" questions correctly!</div><div class='col-12'>You have answered "+lose+" questions incorrectly!</div><div class='col-12' id='col-12'>"+BeginNewButton+"</div></div>");
+        $("#BeginNewButton").on("click", reStart);
+      }
   }else if (x === false){
     lose++;
+      if (StartNumber > 0){
+        game();
+      } else {
+        $("#jumbotron").html("<div class='row'><div class='col-12'>"+Title+"</div><hr><div class='col-12'>You have answered "+win+" questions correctly!</div><div class='col-12'>You have answered "+lose+" questions incorrectly!</div><div class='col-12' id='col-12'>"+BeginNewButton+"</div></div>");
+        $("#BeginNewButton").on("click", reStart);
+      }
   };
-  window.location.replace("result.html");
 };
-//function - subtract 1 from the startNumber, post result to html, and when the start number is = 0 redirects user to result page.
-function subtract(){
-    startNumber--;
-    if (startNumber === 0){
-    stop();
-    window.location.replace("result.html");
-    }
-    $("#displayNumber").html("<h2>" + startNumber + "</h2>");
+function reStart() {
+  StartNumber = 10;
+  win = 0;
+  lose = 0;
+  start();
 };
-//function - variable set "i" to a random number between 0 and 4, "i" will then be used to select a question from the question array and display within html.
-function getRandom(){
-  var i = Math.floor((Math.random() * 4) + 0);
-  $(".questionArea").append("<div>"+questions[i].q+"</div>");
-  $(".questionArea").append(questions[i].a1);
-  $(".questionArea").append(questions[i].a2);
-  $(".questionArea").append(questions[i].a3);
-};
-//function - execute the start function
+
+/////////////////--Function-Execution-/////////////////
 start();
-getRandom();
